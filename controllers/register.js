113eee605555f4b66f31514251d640cd22a79bd2
Cyril Antoni <cyril.lay.antoni@gmail.com>
2018-06-19 10:50:46 +0200

    add controllers/register

---
+++ controllers/register.js
@@ -0,0 +1,30 @@
+const handleRegister = (req, res, db, bcrypt) => {
+  const { name, email, password } = req.body;
+  const hash = bcrypt.hashSync(password);
+  
+  db.transaction(trx => {
+    trx.insert({
+      email: email,
+      hash: hash
+    })
+    .into('login')
+    .returning('email')
+    .then(emailLogin => {
+      return trx('users')
+      .returning('*')
+      .insert({
+      name: name,
+      email: emailLogin[0],
+      joined: new Date()
+      })
+      .then(user => res.json(user[0]))
+    })
+    .then(trx.commit)
+    .catch(trx.rollback);
+  })
+  .catch(err => res.status(400).json('Unable to register!'));
+}
+
+module.exports = {
+  handleRegister: handleRegister,
+};
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
+const register = require('./controllers/register');
+
 const db = knex({
   client: 'pg',
   connection: {
@@ -41,32 +43,7 @@ app.post('/signin', (req, res) => {
   .catch(err => res.status(400).json('unable to get credentials'));
 });
 
-app.post('/register', (req, res) => {
-  const { name, email, password } = req.body;
-  const hash = bcrypt.hashSync(password);
-  
-  db.transaction(trx => {
-    trx.insert({
-      email: email,
-      hash: hash
-    })
-    .into('login')
-    .returning('email')
-    .then(emailLogin => {
-      return trx('users')
-      .returning('*')
-      .insert({
-      name: name,
-      email: emailLogin[0],
-      joined: new Date()
-      })
-      .then(user => res.json(user[0]))
-    })
-    .then(trx.commit)
-    .catch(trx.rollback);
-  })
-  .catch(err => res.status(400).json('Unable to register!'));
-});
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
