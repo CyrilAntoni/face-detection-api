a1f8ef02b14ca8d44fd287769bcc09920e72a50d
Cyril Antoni <cyril.lay.antoni@gmail.com>
2018-06-19 18:26:52 +0200

    update db connection

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-fluffy-53751',
     user : 'postgres',
     password : 'root',
     database : 'face-detection'
