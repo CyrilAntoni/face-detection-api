b7ca00bb5b1b48a4c21907d4095f50997daaf67e
Cyril Antoni <cyril.lay.antoni@gmail.com>
2018-06-19 17:26:00 +0200

    add server process.env.PORT

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.put('/image', (req, res) => {image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res) => {image.handleImageApi(req, res)});
 
 
-app.listen(3001, () => {
+app.listen(process.env.PORT || 3001, () => {
   console.log('server running...');
 });
