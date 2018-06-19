6e1aa4cccb6eea47420f43ad4615271a0bc82dc2
Cyril Antoni <cyril.lay.antoni@gmail.com>
2018-06-19 16:46:22 +0200

    secure API key

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,16 @@
+const Clarifai = require('clarifai');
+
+//initialize Clarifai API
+const app = new Clarifai.App({
+ apiKey: 'c6f23f1a98ab4496982d059e4aba3c7d'
+});
+
+const handleImageApi = (req, res) => {
+  app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+  .then(data => res.json(data))
+  .catch(err => res.status(400).json('unable to work with API'));
+};
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users').where('id', '=', id)
@@ -12,4 +25,5 @@ const handleImage = (req, res, db) => {
 
 module.exports = {
   handleImage,
+  handleImageApi,
 };
