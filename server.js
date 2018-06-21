6c84a78cb9256a4c59013e39ec429a7e8bb42442
Cyril Antoni <cyril.lay.antoni@gmail.com>
2018-06-21 08:18:16 +0200

    add ping to server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const http = require ('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
@@ -9,6 +10,8 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+
+
 const db = knex({
   client: 'pg',
   connection: {
@@ -16,6 +19,13 @@ const db = knex({
     ssl: true,
   }
 });
+
+// ping the app every 5 minutes so the app stays awake on heroku
+setInterval(() => {
+  http.get('https://face-detection-api-ca.herokuapp.com/');
+  http.get('https://face-detection-cyril-antoni.herokuapp.com/');
+  console.log('app pinged!');
+}, 300000);
 
 const app = express();
 app.use(bodyParser.json());
