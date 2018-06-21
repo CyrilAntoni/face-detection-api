1b9573f150e5d3bd88a3602cfdae2fbdff2f6fdb
Cyril Antoni <cyril.lay.antoni@gmail.com>
2018-06-21 08:44:45 +0200

    switch ping to https

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const http = require ('http');
+const https = require ('https');
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
@@ -22,8 +22,8 @@ const db = knex({
 
 // ping the app every 5 minutes so the app stays awake on heroku
 setInterval(() => {
-  http.get('https://face-detection-api-ca.herokuapp.com/');
-  http.get('https://face-detection-cyril-antoni.herokuapp.com/');
+  https.get('https://face-detection-api-ca.herokuapp.com/');
+  https.get('https://face-detection-cyril-antoni.herokuapp.com/');
   console.log('app pinged!');
 }, 300000);
 
